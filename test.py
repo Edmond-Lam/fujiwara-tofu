09a62c63323519eb3a8bcb766fb91eeb8a1d5139
fhaque1 <farhan3231@gmail.com>
2017-04-30 16:49:22 -0400

    done

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
+import time
+
+def func_time(f):
+	start = time.time()
+	def inner(*arg):
+		end = time.time()
+		print "execution time: " + str(end - start)
+		return f(*arg)
+	return inner
+
 def func_args( f ):
    def inner( *arg ):
        print f.func_name + ": " + str(arg)
        return f(*arg)
    return inner
 
-#def foo( x, y, z ):
-  
-#  return [x+5,y+6,z]
-
-#closure = wrapper(foo)
-#print closure( 10000,6,"wow" ) #will run foo with the arguments -2, 3, 'hello' through wrapper
-
-
+@func_time
 @func_args
 def fibo(n):
     if n < 2:
@@ -20,4 +23,4 @@ def fibo(n):
        return fibo(n-1)+fibo(n-2)
 
 
-print fibo(10)
+print fibo(5)
